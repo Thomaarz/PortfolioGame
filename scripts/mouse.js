2ab76735b7d814d76f7de2e04aff4a776ed2f30f
Thomaarz <49363524+Thomaarz@users.noreply.github.com>
2022-10-05 21:39:08 +0200

    Delete scripts directory

--- scripts/mouse.js
+++
@@ -1,9 +0,0 @@
-window.onmousemove = function (event) {
-    const mouse_x = event.clientX;
-    const mouse_y = event.clientY;
-
-    var cursor = document.getElementById("cursor");
-
-    cursor.style.left = (mouse_x - 20) + 'px';
-    cursor.style.top = (mouse_y - 10) + 'px';
-}
